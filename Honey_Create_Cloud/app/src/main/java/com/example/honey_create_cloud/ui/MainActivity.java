1a11941287d4d843c64c79ad1d3de0a58b4a971f
Your Name <wang18612520132@163.com>
2020-07-15 17:23:23 +0800

    phone  切换生产环境

--- Honey_Create_Cloud/app/src/main/java/com/example/honey_create_cloud/ui/MainActivity.java
+++ Honey_Create_Cloud/app/src/main/java/com/example/honey_create_cloud/ui/MainActivity.java
@@ -845,10 +845,10 @@ public class MainActivity extends AppCompatActivity {
      */
     private Connection getConnection() {
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("119.3.28.24");//主机地址：192.168.1.105
-        factory.setPort(5672);// 端口号:5672
-        factory.setUsername("honeycomb");// 用户名
-        factory.setPassword("honeycomb");// 密码
+        factory.setHost("122.112.141.135");//主机地址：192.168.1.105
+        factory.setPort(25672);// 端口号:25672
+        factory.setUsername("honeycom");// 用户名
+        factory.setPassword("wu168@QqFn2019");// 密码
         factory.setVirtualHost("/");
         try {
             return factory.newConnection();
@@ -1192,7 +1192,7 @@ public class MainActivity extends AppCompatActivity {
                     final MediaType mediaType = MediaType.parse("image/jpeg");//创建媒房类型
                     builder.addFormDataPart("fileObjs", file.getName(), RequestBody.create(mediaType, file));
                     builder.addFormDataPart("fileNames", "");
-                    builder.addFormDataPart("bucketName", Constant.test_bucket_Name);
+                    builder.addFormDataPart("bucketName", Constant.prod_bucket_Name);
                     builder.addFormDataPart("folderName", "headPic");
                     MultipartBody requestBody = builder.build();
                     final Request request = new Request.Builder()
